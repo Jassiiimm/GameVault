1178dd88073050be3924b12217f54005cbd5e303
Jassiiimm <owainatijassim@gmail.com>
2026-07-20 23:53:54 +0300

    Created game model

---
+++ models/game.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const gameSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        platform: {
+            type: String,
+            required: true,
+        },
+        image: String,
+        description: String,
+        status: String,
+        isPublic: Boolean,
+        genre: String,
+        hoursPlayed: Number,
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const Game = mongoose.model('Game', gameSchema)
+
+module.exports = Game
